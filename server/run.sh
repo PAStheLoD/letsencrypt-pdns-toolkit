04b57c4fbc49a14f525b86ca09904b90049ca374
Pas <pasthelod@gmail.com>
2016-07-25 18:01:19 +0200

    Update run.sh

--- server/run.sh
+++ server/run.sh
@@ -2,8 +2,15 @@
 
 cd $(dirname $(readlink -f $0))
 
-if [[ ! -r cert.pem ]] || [[ ! -r key.pem ]] ; then
-    openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days XXX -subj '/CN=le-crypt' -nodes
+if [[ ! -r cert.pem ]] || [[ ! -r cert.key ]] ; then
+    openssl req -x509 -newkey rsa:2048 -keyout cert.key -out cert.pem -days 720 -subj '/CN=le-crypt' -nodes || { echo "Error while generating server keypair/cert"; exit 1; }
 fi
 
-uwsgi --http 0.0.0.0:8888 --https 0.0.0.0:8443,cert.pem,key.pem  --threads 2 -w le-api:app --set-ph config-file=le-config.json
+uwsgi=$(which uwsgi 2>/dev/null)
+
+if [[ "$uwsgi" = "" ]] || [[ ! -x "$uwsgi" ]] ; then
+    echo "Cannot found working uwsgi"
+    exit 1
+fi
+
+exec uwsgi --http 0.0.0.0:8888 --https 0.0.0.0:8443,cert.pem,cert.key --threads 2 -w le-api:app --set-ph config-file=le-config.json
