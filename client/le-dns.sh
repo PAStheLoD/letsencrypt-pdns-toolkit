b3aa62787de10827abecd47e5149f8a1e7954426
pas <pas@zomg.hu>
2017-11-10 04:49:22 +0100

    yeey, new version for dehydrated

--- client/le-dns.sh
+++ client/le-dns.sh
@@ -4,6 +4,7 @@
 # https://github.com/antoiner77/letsencrypt.sh-pdns/blob/master/pdns.sh
 #
 
+# set -x
 set -e
 set -u
 set -o pipefail
@@ -16,105 +17,131 @@ if [[ ! -r le.config ]] ; then
     exit 1
 fi
 
+api_server="$(grep -Po '^(?!(\s*#+)+)\s*api_server="?\Khttps://[a-z0-9.-]+(:[0-9]+)?(?="?$)' le.config || true)"
 
-if [[ $(grep -Po '^(?!(\s*#+)+)\s*api_server=\K.*$' le.config) = "" ]] ; then
-    echo "API Server not configured (missing api_server=\"...\" from le.config)"
+if [[ $api_server = "" ]] ; then
+    echo "API Server not configured (missing api_server=\"https://...\" from le.config)"
     exit 1
 fi
 
-api_server=$(grep -Po '^(?!(\s*#+)+)\s*api_server="?\K[^"]+(?="?$)' le.config)
+api_server_cert="$(grep -Po '^(?!(\s*#+)+)\s*api_server_cert="?\K[^"]+(?="?$)' le.config || true)"
 
+function test_server() {
+    local url="${1}"
+    local cert_hack="${2}"
 
-api_server_cert="$(grep -Po '^(?!(\s*#+)+)\s*api_server_cert="?\K[^"]+(?="?$)' le.config)"
+    # shellcheck disable=SC2086
+    curl_test_output="$(curl -sv "${url}" $cert_hack -I 2>&1)"
 
-if [[ "$api_server_cert" != "" ]] ; then
-    if [[ -r "$api_server_cert" ]] ; then
+    if [[ $(echo "$curl_test_output" | grep -c 'server certificate verification failed') != 0 ]] ; then
+        echo "ERROR: API server TLS verification failed :("
+        openssl s_client -connect "$server_host:$port" |& grep 'Verify return code:'
+        exit 1
+    fi
+
+    if [[ $(echo "$curl_test_output" | grep -Poc  '^< HTTP/1\.1' || true) = 0 ]] ; then
+        echo "ERROR: API server cannot be reached"
+        exit 1
+    fi
+}
+
+[[ "$api_server_cert" != "" ]] && {
+    [[ -r "$api_server_cert" ]] && {
         if [[ $(echo "$api_server" | grep -Pc ':[0-9]+$') = 1 ]] ; then
             port=$(echo "$api_server" | grep -Po ':\K[0-9]+$')
         else
-            port=8443
+            port=443
         fi
 
-        server_host=$(echo "$api_server" | grep -Po '[^:]+' | head -n1)
+        server_host=$(echo "$api_server" | grep -Po 'https://\K[a-z0-9.-]+' | head -n1)
 
         if [[ $(echo "$server_host" | grep -P '^([0-9]+\.)+[0-9]+$') = "$server_host" ]] ; then
             ca="--resolve le-crypt:$port:$server_host --cacert $api_server_cert"
         else
             ca="--resolve le-crypt:$port:$(dig +short "$server_host") --cacert $api_server_cert"
         fi
-        scheme="https"
-        api_server="le-crypt:$port"
-        
-        # shellcheck disable=SC2086 
-        curl_test_output="$(curl -sv "${scheme}://${api_server}/api/" $ca -I 2>&1)"
+        api_server_url="https://le-crypt:$port/api/"
 
-        if [[ $(echo "$curl_test_output" | grep -c 'server certificate verification failed') != 0 ]] ; then
-            echo "ERROR: API server TLS verification failed :("
-            openssl s_client -connect "$server_host:$port" |& grep 'Verify return code:'
-            exit 1
-        fi
+        test_server "$api_server_url" "$ca"
 
-        if [[ $(echo "$curl_test_output" | grep -Poc  '^< HTTP/1\.1') = 0 ]] ; then
-            echo "ERROR: API server cannot be reached"
-            exit 1
-        fi
-    else
+    } || {
         echo "api_server_cert is set but not readable"
         exit 1
-    fi
-else
-    scheme="http"
+    }
+} || {
+    # try the server, maybe it has a valid cert
+    api_server_url="${api_server}/api/"
+
+    test_server "$api_server_url"
+
     ca=""
-fi
+}
 
 echo "DEBUG: args: $*"
 
-if [[ ! "$#" -gt 3 ]] ; then
+[[ ! "$#" -gt 0 ]] && {
+    echo "usage: ./$0 [mode]"
     echo "usage: ./$0 [mode] [domain] \"\" [token!]"
     exit 0
-fi
+}
+
+[[ "$1" = "startup_hook" ]] || [[ "${1}" = "exit_hook" ]] || [[ "${1}" = "unchanged_cert" ]] || [[ "${1}" = "deploy_cert" ]] && {
+    # do nothing for now
+    exit 0
+}
+
+[[ ! "$#" -gt 3 ]] && {
+    echo "usage: ./$0 [mode] [domain] \"\" [token!]"
+    exit 0
+}
 
 domain="${2}"
 token="${4}"
 
-if [[ $(grep -Po "domain=\"$domain\"" le.config | wc -l) != 1 ]] ; then
+[[ $(grep -Po "domain=\"$domain\"" le.config | wc -l) != 1 ]] && {
     echo "domain=\"$domain\" must be configured in le.config"
     exit 1
-fi
+}
 
-key="$(grep -Po "^(?!(\s*#+)+)\s*domain=\"?$domain\"?\s+key=\"?\K[^\"]+(?=\"?(\s+|\$))" le.config)"
+key="$(grep -Po "^(?!(\s*#+)+)\s*domain=\"?$domain\"?\s+key=\"?\K[^\"]+(?=\"?(\s+|\$))" le.config || true)"
+
+[[ "$key" = "" ]] && {
+    echo "Missing key=\"...\" from config for domain: $domain"
+    exit 1
+}
 
 done="no"
 
-if [[ "$1" = "deploy_challenge" ]]; then
-  curl_output=$(curl -s "${scheme}://${api_server}/api/_acme-challenge.${domain}" $ca -d "$token" -H "API-Key: $key")
-  if [[ $(echo "$curl_output" | grep -c "^ok$") = 0 ]] ; then
-      echo "ERROR: failed to deploy challenge :/"
-      exit 1
-  fi
+[[ "$1" = "deploy_challenge" ]] && {
+  curl_output=$(curl -s "${api_server_url}_acme-challenge.${domain}" $ca -d "$token" -H "API-Key: $key")
+  echo "$curl_output" | grep -q "^ok$" || {
+      echo "$curl_output" | grep -qi "unauthorized" && {
+          echo "wrong key for domain"
+          exit 1
+      }
 
-  while ! dig +trace @8.8.8.8 -t TXT "_acme-challenge.${domain}" | grep -- "$token" > /dev/null
-    do
+      echo "ERROR: failed to deploy challenge :/"
+      echo "$curl_output"
+      exit 1
+  }
+
+  while ! dig +trace @8.8.8.8 -t TXT "_acme-challenge.${domain}" | grep -- "$token" > /dev/null ; do
        printf "."
        sleep 3
-    done
-   done="yes"
-fi
+  done
 
-if [[ "$1" = "clean_challenge" ]]; then
-    curl -s -X DELETE "${scheme}://${api_server}/api/_acme-challenge.${domain}" $ca -d "$token" -H "API-Key: $key"
+  done="yes"
+}
+
+[[ "$1" = "clean_challenge" ]] || [[ "${1}" = "invalid_challenge" ]] || [[ "${1}" = "request_failure" ]] && {
+    curl -s -X DELETE "${api_server_url}_acme-challenge.${domain}" $ca -d "$token" -H "API-Key: $key"
 
     done="yes"
-fi
+}
 
-if [[ "${1}" = "deploy_cert" ]]; then
-    # do nothing for now
-    done="yes"
-fi
-
-if [[ ! "${done}" = "yes" ]]; then
+[[ ! "${done}" = "yes" ]] && {
     echo Unkown hook "${1}"
     exit 1
-fi
+}
 
 exit 0
